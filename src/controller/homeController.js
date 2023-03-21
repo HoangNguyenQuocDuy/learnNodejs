a04af4b386b7b7415d0b25d36aa371de3006e4f2
Quoc Duy <quocduy6114@gmail.com>
2023-03-21 20:43:42 +0700

    delete and update user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -12,9 +12,9 @@ const getHomePage = async (req, res) => {
 const getDetailPage = async (req, res) => {
   let id = req.params.id;
 
-  const [user] = await promisePool.execute(
-    `SELECT * FROM users where id = ${id}`
-  );
+  const [user] = await promisePool.execute("SELECT * FROM users where id = ?", [
+    id,
+  ]);
 
   return res.send(JSON.stringify(user));
 };
@@ -23,15 +23,46 @@ const createNewUser = async (req, res) => {
   console.log(">>> check req: ", req.body);
   const { firstName, lastName, email, address } = req.body;
   await promisePool.execute(
-    'INSERT INTO users(firstName, lastName, email, address) VALUES (?, ?, ?, ?)',
+    "INSERT INTO users(firstName, lastName, email, address) VALUES (?, ?, ?, ?)",
     [firstName, lastName, email, address]
   );
 
-  return res.redirect('/')
+  return res.redirect("/");
+};
+
+const deleteUser = async (req, res) => {
+  let id = req.body.id;
+  promisePool.execute("DELETE FROM users WHERE id = ?", [id]);
+
+  return res.redirect("/");
+};
+
+const getUser = async (req, res) => {
+  const id = req.params.id;
+
+  const [user] = await promisePool.execute("SELECT * FROM users where id = ?", [
+    id,
+  ]);
+
+  return res.render("update.ejs", { dataUser: user[0] }); // x <- y
+};
+
+const updateUser = async (req, res) => {
+  const { firstName, lastName, email, address, id } = req.body;
+
+  await promisePool.execute(
+    "UPDATE users SET firstName=?, lastName=?, email=?, address=? WHERE id=?",
+    [firstName, lastName, email, address, id]
+  );
+
+  return res.redirect('/');
 };
 
 module.exports = {
   getHomePage,
   getDetailPage,
   createNewUser,
+  deleteUser,
+  getUser,
+  updateUser,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 const initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
-
     router.get('/about', (req, res) => {
         res.send('about')
     })
-
     router.get('/detail/user/:id', homeController.getDetailPage)
-
     router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser)
+    router.get('/update-user/:id', homeController.getUser)
+    router.post('/update-user', homeController.updateUser)
 
     return app.use('/', router)
 }
